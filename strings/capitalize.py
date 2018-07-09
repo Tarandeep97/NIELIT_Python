fa225240b84a7c627272d0e0ba16c84b37504603
Tarandeep Singh <tarandeeps1197@gmail.com>
2018-07-09 18:26:10 +0530

    Update capitalize.py

--- strings/capitalize.py
+++ strings/capitalize.py
@@ -1,2 +1,3 @@
-string = input()
+#This Code inputs a string and prints string with each words first character in caps.
+string = input()  
 print(' '.join(i.capitalize() for i in string.split(" ")))
