204ba9001de349c17f6f96c7b9d4f7f856b19eeb
Tarandeep Singh <tarandeeps1197@gmail.com>
2018-07-22 14:57:39 +0530

    Update fibopy.py

--- fibopy.py
+++ fibopy.py
@@ -3,10 +3,13 @@ f1=1
 fnxt=0
 print(f0)
 print(f1)
+n = int(input())
 while f1<50:
     fnxt=f0+f1
     f0=f1
     f1=fnxt
-    if fnxt>34:
+    if fnxt<=50:
+        print(fnxt)
+    else:
         break
     print(fnxt)
